706fffb1dbce539de8d9ad57b0e1afeebe10634f
snifftont@gmail.com <snifftont@gmail.com>
2024-07-07 05:20:29 -0700

    Blazor Forms

---
+++ EmployeeManagement.Web/EmployeeManagement.Web/Components/Pages/EditEmployeeBase.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Components.Pages.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Components.Pages
+{
+    public class EditEmployeeBase:ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee employee { get; set; } = new Employee();
+        
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            
+            employee = await EmployeeService.GetEmployee(int.Parse(Id));
+            
+
+        }
+    }
+}
--- EmployeeManagement.Web/EmployeeManagement.Web/Components/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/EmployeeManagement.Web/Components/Pages/EmployeeDetailsBase.cs
@@ -1,12 +1,18 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Components.Pages.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Connections.Features;
 
 namespace EmployeeManagement.Web.Components.Pages
 {
     public class EmployeeDetailsBase:ComponentBase
     {
         public Employee employee { get; set; }= new Employee();
+        protected string coordinates { get; set; }
+        public string ButtonText { get; set; } = "Hide Footer";
+        public string cssClass { get; set; } = null;
+
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
         [Parameter]
@@ -17,5 +23,25 @@ namespace EmployeeManagement.Web.Components.Pages
             Id=Id?? "1";
             employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
+
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            coordinates = $"X ={e.ClientX} Y={e.ClientY}";
+        }
+
+        protected void Button_Click()
+        {
+            if(ButtonText=="Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                cssClass = "displayNone";
+            }
+            else
+            {
+                cssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
+
     }
 }
