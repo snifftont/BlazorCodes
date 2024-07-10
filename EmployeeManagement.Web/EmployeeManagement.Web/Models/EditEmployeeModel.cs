89fdb4406f38875ef72de0544efd3a049d0b92ef
snifftont@gmail.com <snifftont@gmail.com>
2024-07-10 03:31:24 -0700

    Blazor Forms

---
+++ EmployeeManagement.Web/EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Models.CustomValidators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class EditEmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "FirstName must be provided")]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "snifftont.com", ErrorMessage = "Only snifftont.com is allowed")]
+        public string Email { get; set; }
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBrith { get; set; }
+        public Gender Gender { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; } = new Department();
+    }
+}
